9314d521c43bcba7448c594296424d84273f83ed
FairyDonut <julianna.gorska99@gmail.com>
2023-01-26 22:50:26 +0100

    workrecoredscreate formularz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Firma YXZ</h1
+    <h1 class="text-center " >Panel firmy YXZ</h1>
+        <div class="container text-center">
+            <div class="row row-cols-2 text-center">
+              <div class="col border">Column</div>
+              <div class="col border">Column</div>
+              <div class="col border">Column</div>
+              <div class="col border">Column</div>
+            </div>
+        </div>
 @endsection
--- resources/views/workRecordsCreate.blade.php
+++ resources/views/workRecordsCreate.blade.php
@@ -1,48 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dodaj ewidencję pracy</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header h1">Dodaj ewidencję czasu pracy</div>
+                  <div class="card-body">
+                    <form method="POST" action="/workrecords/create">
+                        @csrf
+                        <div class="row mb-3">
+                            <h3 class="text-center">Pracownicy:</h3>
+                            <label for="sender" class="col-md-4 col-form-label text-md-end">Zlecający:</label>
+                            <div class="col-md-6">
+                                <select name="sender" id="sender" class="form-control">
+                                    @foreach ($users as $user)
+                                        <option value={{$user->id}}>{{$user->firstName}}</option>
+                                    @endforeach
+                                </select>
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="worker" class="col-md-4 col-form-label text-md-end">Pracownik:</label>
+                            <div class="col-md-6">
+                                <select name="worker" id="worker" class="form-control">
+                                    @foreach ($users as $user)
+                                        <option value={{$user->id}}>{{$user->firstName}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
 
-<form method="POST" action="/workrecords/create">
-    @csrf
-    <h3>Pracownicy:</h3>
-
-    <label for="sender">Zlecający:</label>
-    <select name="sender" id="sender">
-        @foreach ($users as $user)
-            <option value={{$user->id}}>{{$user->firstName}}</option>
-        @endforeach
-    </select>
-
-    <label for="worker">Pracownik:</label>
-    <select name="worker" id="worker">
-        @foreach ($users as $user)
-            <option value={{$user->id}}>{{$user->firstName}}</option>
-        @endforeach
-    </select>
-
-    <h3>Start pracy:</h3>
-
-    <label for="work_start_date">Data:</label>
-    <input type="date" name="work_start_date" id="work_start_date" value={{Carbon\Carbon::now()}}>
-
-    <label for="work_start_time">Czas:</label>
-    <input type="time" name="work_start_time" id="work_start_time">
-
-    <h3>Koniec pracy:</h3>
-
-    <label for="work_end_date">Data:</label>
-    <input type="date" name="work_end_date" id="work_end_date" value={{Carbon\Carbon::now()}}>
-
-    <label for="work_end_time">Czas:</label>
-    <input type="time" name="work_end_time" id="work_end_time">
-
-    <p><button type="submit">Zapisz</button></p>
-
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div>{{ $error }}</div>
-        @endforeach
-    @endif
-  </form>
+                        <div class="row mb-3">
+                        <h3 class="text-center">Start pracy:</h3>
+                        <label for="work_start_date" class="col-md-4 col-form-label text-md-end">Data:</label>
+                        <div class="col-md-6">
+                        <input type="date" name="work_start_date" id="work_start_date" class="form-control" value={{Carbon\Carbon::now()}}>
+                        </div>
+                        </div>
+                        <div class="row mb-3">
+                        <label for="work_start_time" class="col-md-4 col-form-label text-md-end">Czas:</label>
+                        <div class="col-md-6">
+                        <input type="time" name="work_start_time" id="work_start_time" class="form-control">
+                        </div>
+                        </div>
+                        <div class="row mb-3">
+                        <h3 class="text-center">Koniec pracy:</h3>
+                        <label for="work_end_date" class="col-md-4 col-form-label text-md-end">Data:</label>
+                        <div class="col-md-6">
+                        <input type="date" name="work_end_date" id="work_end_date" class="form-control" value={{Carbon\Carbon::now()}}>
+                        </div>
+                        </div>
+                        <div class="row mb-3">
+                        <label for="work_end_time" class="col-md-4 col-form-label text-md-end">Czas:</label>
+                        <div class="col-md-6">
+                        <input type="time" name="work_end_time" id="work_end_time" class="form-control">
+                        </div>
+                        </div>
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">Zapisz</button>
+                            </div>
+                        </div>
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div>{{ $error }}</div>
+                            @endforeach
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
